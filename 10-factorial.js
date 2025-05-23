534d13d7cf4997eeac2bb378878cc92818e5d04f
codebyemma <mysubbacc@gmail.com>
2025-05-23 10:03:52 +0100

    This commit include my advance tast, which i can calculate the factorial of any number

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,3 +1,16 @@
 const {argv} = require('node:process');
 
+let num = Number(argv[2]);
 
+function factorial(n) {
+	if (n === 1 || isNaN(n)) {
+		return 1;
+	}
+	else
+	{
+		let pro = factorial(n - 1) * n;
+		return pro;
+	}
+}
+
+console.log(factorial(num));
